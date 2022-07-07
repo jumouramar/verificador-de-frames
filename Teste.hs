99cf907ffe7661cf06f411e8866206101b649c5a
Juliana Moura <juliana_moura@id.uff.br>
2022-07-07 16:04:32 -0300

    passando parâmetros

--- Teste.hs
+++ Teste.hs
@@ -2,5 +2,14 @@ module Teste (
   chamaFuncao
 ) where
 
-chamaFuncao :: Int -> String
+chamaFuncao :: [(Char, Char, Char)] -> programPDL -> String
 chamaFuncao x = "Está contido"
+
+
+--chooseOperator :: [Char] -> [Char]
+--chooseOperator lista
+--  | head lista == ';' = "função para ;"
+--  | head lista == 'U' = "função para U"
+--  | head lista == '*' = "função para *"
+-- | head lista == '(' = "função para tratar ("
+-- | otherwise chooseOperator tail lista
--- main.hs
+++ main.hs
@@ -1,20 +1,24 @@
 import Teste
+import util
 
 main = do
   
-  putStrLn $ "FRAME: [(1,2,a)] PROGRAM: a"
-  putStrLn $ chamaFuncao 1
-  putStrLn $ "\n"
   putStrLn $ "FRAME: [(1,2,a),(2,3,b)] PROGRAM: a;b"
-  putStrLn $ chamaFuncao 1
+  putStrLn $ chamaFuncao [('1','2','a'),('2','3','b')] [(';'('a','b'))]
   putStrLn $ "\n"
-  putStrLn $ "FRAME: [(1,2,a)] PROGRAM: aUb"
-  putStrLn $ chamaFuncao 1
-  putStrLn $ "\n"
-  putStrLn $ "FRAME: [(1,2,b)] PROGRAM: aUb"
-  putStrLn $ chamaFuncao 1
-  putStrLn $ "\n"
-  putStrLn $ "FRAME: [(1,1,a)] PROGRAM: a*"
-  putStrLn $ chamaFuncao 1
-  putStrLn $ "\n"
+
+ -- putStrLn $ "FRAME: [(1,2,a)] PROGRAM: aUb"
+  --let ex2 :: [Char] [Char]
+  --let ex2 = chamaFuncao [('1','2','a')] --['U'('a','b')]
+  --putStrLn $ "\n"
+
+ -- putStrLn $ "FRAME: [(1,2,b)] PROGRAM: aUb"
+  --ex3 :: [Char] [Char]
+  --let ex3 = chamaFuncao [('1','2','b')] --['U'('a','b')]
+ -- putStrLn $ "\n"
+
+ -- putStrLn $ "FRAME: [(1,1,a)] PROGRAM: a*"
+  --ex4 :: [Char] [Char]
+  --let ex4 = chamaFuncao [('1','1','a')] --['*'('a')]
+ -- putStrLn $ "\n"
   
